0405869cc54c05198c9bbb073481bd8833cf5d80
Github - Actions <26508358+Sergeydigl3@users.noreply.github.com>
2021-12-15 13:13:11 +0000

    Auto push from master to pages

--- releases/answers-script.user.js
+++ releases/answers-script.user.js
@@ -1,13 +1,12 @@
 // ==UserScript==
 // @name         LMS GUAP Answers
-// @version      2.3.2
+// @version      2.3.3
 // @description  moodle test answers!
 // @author       Sergeydigl3
 // @match        *://school.moodledemo.net/*
 // @match        *://lms.guap.ru/*
-// @updateURL    https://raw.githubusercontent.com/Sergeydigl3/moodle-answers-script/main/answers-script.meta.js
-// @downloadURL  https://raw.githubusercontent.com/Sergeydigl3/moodle-answers-script/main/answers-script.user.js
-// @supportURL   https://github.com/Sergeydigl3/moodle-answers-script
+// @updateURL    https://Sergeydigl3.github.io/releases/answers-script.user.js
+// @downloadURL  https://Sergeydigl3.github.io/releases/answers-script.user.js
 // @require      https://cdn.jsdelivr.net/npm/@fingerprintjs/fingerprintjs@3/dist/fp.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js
 // @require      https://cdn.socket.io/4.1.2/socket.io.min.js
